85e15b6e1b32f959804c7ec92db4093b5ea47d6b
Hitalo Alves <hitalo.ralves@hotmail.com>
2020-05-15 16:27:31 -0300

    finalizando ajustes no salto e iniciando config de atack - Hitalo

--- MainWorld/Assets/Scripts/PlayerController.cs
+++ MainWorld/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Animator playerAnimator;
-    private Rigidbody2D playerRB;
-    private SpriteRenderer sRender;
+    private         Animator        playerAnimator;
+    private         Rigidbody2D     playerRB;
+    private         SpriteRenderer  sRender;
 
     [Header("Configuração de movimentação")]
-    private float h;//variaveil de movimento horizontal 
-    private float v;//variaveil de movimento vertical
-    public Collider2D collisorEmPé;// Collisor em pé 
-    public Collider2D collisorAbaixado;//Collisor abaixado
-    public bool Grounded; //indica se o player esta no chao
-    public LayerMask oqueEhChao; //indica o que é superficie para o teste do grounded
-    public Transform groundCheck; //objeto responsavel por fazer a detecção para vermos se estamos tocando o chão
-    public float jumpForce; //força aplicada para gerar o pulo do personagem
-    public float speed; // velocidade de movimento do personagem
-    public bool lookLeft;//indica se o personagem esta olhando para a esquerda
-    public bool atacando; //indica que o personagem esta atacando
-    public int IdAnimation; //indica o id da animação
-    private float x;//pega o scale.x do player
-    public bool validarMovimentoX =false;
+    private         float           h;//variavel de movimento horizontal 
+    public          float           speed; // velocidade de movimento do personagem
+    private         float           v;//variavel de movimento vertical
+    public          bool            atacando; //indica que o personagem esta atacando
+    public          int             IdAnimation; //indica o id da animação
+    private         bool            habilitarMovimentacaoPlayer =true;
+    
+    public          Collider2D      collisorEmPé;// Collisor em pé 
+    public          Collider2D      collisorAbaixado;//Collisor abaixado
+    
+    public          bool            Grounded; //indica se o player esta no chao
+    public          LayerMask       oqueEhChao; //indica o que é superficie para o teste do grounded
+    public          Transform       groundCheck; //objeto responsavel por fazer a detecção para vermos se estamos tocando o chão
+    
+    public          float           jumpForceY = 15; //força aplicada no eixo y para gerar o pulo do personagem
+    public          float           jumpForceX = 6;//força aplicada no eixo x para gerar o pulo do personagem
+    
+    public          bool            lookLeft;//indica se o personagem esta olhando para a esquerda
+    private         float           x;//pega o scale.x do player
+
+    
+    [Header("Configuração de Interação com inimigos")]
+   
+    private Vector3 cabecaScan  = Vector3.up;
+    private Vector3 ombroScan = new Vector3(0.136f, 0.143f, 0);
+    private Vector3 toraxScan = Vector3.right;
+    private Vector3 cinturaScan = Vector3.right;
+    private Vector3 joelhoScan = Vector3.right;
+    private Vector3 peScan = Vector3.right;
+
+    public Transform scanRayCabeca;
+    public Transform scanRayOmbro;
+    public Transform scanRayTorax;
+    public Transform scanRayCintura;
+    public Transform scanRayJoelho;
+    public Transform scanRayPe;
+    
+    public LayerMask interacao;
+    public GameObject objetoInteração;
+
+    [Header("Configuração de Ataque")]
+    public GameObject colliderAtak1;
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
@@ -31,12 +59,13 @@ public class PlayerController : MonoBehaviour
         x = transform.localScale.x;
     }
 
+  
     private void FixedUpdate()
     {
         Grounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f, oqueEhChao);//esse teste so deve acontecer se houver uma colisao com a layer Ground
 
         //movimentar o personagem
-        if (!validarMovimentoX)
+        if (!habilitarMovimentacaoPlayer)
             return;
         playerRB.velocity = new Vector3(h * speed, playerRB.velocity.y);
         h = Input.GetAxisRaw("Horizontal");//capta a entra dos cursores seta direita e seta esquerda
@@ -62,8 +91,7 @@ public class PlayerController : MonoBehaviour
             {
                 h = 0; //quando o personagem estiver em posição de defesa, ele não poderá se movimentar para frente
             }
-        }
-        else if (h != 0)
+        }else if (h != 0)
         {
             IdAnimation = 1;
         }
@@ -72,7 +100,7 @@ public class PlayerController : MonoBehaviour
             IdAnimation = 0;
         }
 
-
+        //inputs para movimentação
         if (Input.GetButtonDown("Fire1") && v >= 0 && atacando == false)
         {
             playerAnimator.SetTrigger("attack1");
@@ -86,21 +114,20 @@ public class PlayerController : MonoBehaviour
             playerAnimator.SetTrigger("attack3");
         }
         if (Input.GetButtonDown("Jump") && Grounded == true)
-        {
-            validarMovimentoX = false;
-            /*
-            if(lookLeft == true && transform.localScale.x > 0)
-            {
-                Flip();
-            }else if(lookLeft == false && transform.localScale.x < 0)
-            {
-                Flip();
-            }*/
-
-            playerRB.AddForce(new Vector2(6 * x,15));
-            StartCoroutine("testando");
+        {//salto lateral
+            habilitarMovimentacaoPlayer= false;
+            playerRB.AddForce(new Vector2(jumpForceX * x,jumpForceY));
+            StartCoroutine("ValidarMovimentoPlayer");
             
         }
+        if(Input.GetKeyDown(KeyCode.V) && Grounded == true)
+        {//pulo simples 
+                
+                playerRB.AddForce(new Vector2(0, jumpForceY));
+              
+            
+        }
+        
 
         if (atacando == true && Grounded == true)
         {
@@ -126,6 +153,9 @@ public class PlayerController : MonoBehaviour
         playerAnimator.SetInteger("idAnimation", IdAnimation);
         playerAnimator.SetFloat("speedY", playerRB.velocity.y);
 
+        //RAYCAST
+        interagir();
+
     }
 
 
@@ -139,6 +169,12 @@ public class PlayerController : MonoBehaviour
 
         transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
 
+        cabecaScan.x = cabecaScan.x * -1;
+        ombroScan.x = ombroScan.x * -1;
+        toraxScan.x = toraxScan.x * -1;
+        cinturaScan.x = cinturaScan.x * -1;
+        joelhoScan.x = joelhoScan.x * -1;
+        peScan.x = peScan.x * -1;
 
     }
     void atack(int atk)
@@ -156,9 +192,56 @@ public class PlayerController : MonoBehaviour
     }
     
     
-    IEnumerator testando()
+    IEnumerator ValidarMovimentoPlayer()
     {
         yield return new WaitForSeconds(0.8f);
-        validarMovimentoX = true;
+        habilitarMovimentacaoPlayer = true;
+    }
+
+    void interagir()
+    {
+        RaycastHit2D scanCabeca = Physics2D.Raycast(scanRayCabeca.transform.position, cabecaScan, 0.2f, interacao);
+        Debug.DrawRay(scanRayCabeca.transform.position, cabecaScan * 0.17f, Color.red);
+
+        RaycastHit2D scanOmbro = Physics2D.Raycast(scanRayOmbro.transform.position, ombroScan, 1.1f, interacao);
+        Debug.DrawRay(scanRayOmbro.transform.position, ombroScan * 1.1f, Color.green);
+
+        RaycastHit2D scanTorax = Physics2D.Raycast(scanRayTorax.transform.position, toraxScan, 0.4f, interacao);
+        Debug.DrawRay(scanRayTorax.transform.position, toraxScan * 0.4f, Color.yellow);
+
+        RaycastHit2D scanCintura = Physics2D.Raycast(scanRayCintura.transform.position, cinturaScan, 0.4f, interacao /*faz com que teste a colisao somente em objetos que tiverem essa layer*/);
+        Debug.DrawRay(scanRayCintura.transform.position, cinturaScan * 0.4f, Color.blue);//faz com que seja visivel a linha seja visivel para teste
+
+        RaycastHit2D scanJoelho = Physics2D.Raycast(scanRayJoelho.transform.position, joelhoScan, 0.3f, interacao);
+        Debug.DrawRay(scanRayJoelho.transform.position, joelhoScan * 0.3f, Color.cyan);
+
+        RaycastHit2D scanPe = Physics2D.Raycast(scanRayPe.transform.position, peScan, 0.38f, interacao);
+        Debug.DrawRay(scanRayPe.transform.position, peScan * 0.38f, Color.black);
+       
+        if (scanCabeca == true || scanOmbro == true || scanTorax == true || scanCintura == true)
+        {
+                playerAnimator.SetTrigger("attack1");
+
+        }else if(scanJoelho == true || scanPe == true)
+        {
+            playerAnimator.SetTrigger("attack3");
+        }
+    }
+
+    void habilitarColliderAtak(int tipoAtaque)
+    {
+
+        print("Entrei");
+       
+            colliderAtak1.SetActive(true);
+        
+    }
+
+    void desabilitarColliderAtak(string tipoAtaque)
+    {
+        if(tipoAtaque == "attack1")
+        {
+            colliderAtak1.SetActive( false);
+        }
     }
 }
