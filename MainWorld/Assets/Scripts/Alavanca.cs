6818d2d5700780e60f60e61c59c24291642388df
Hitalo Alves <hitalo.ralves@hotmail.com>
2020-10-19 16:34:05 -0300

    Ajustando a parte 3 da fase 8

---
+++ MainWorld/Assets/Scripts/Alavanca.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alavanca : MonoBehaviour
+{
+    public Animator gradeAnimator;
+    public GameObject alavancaDestravada;
+    public GameObject alavancaTravada;
+    void Start()
+    {
+        alavancaTravada.SetActive(true);
+        alavancaDestravada.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.gameObject.tag)
+        {
+            case "Player":
+                alavancaTravada.SetActive(false);
+                alavancaDestravada.SetActive(true);
+                gradeAnimator.SetTrigger("subirGrade");
+                break;
+        }
+    }
+}
