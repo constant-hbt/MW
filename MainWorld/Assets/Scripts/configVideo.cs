168eef0aff59a10975916c012d5ce4535c246542
YOUR NAME <YOUR EMAIL ADDRESS>
2020-10-22 22:29:28 -0200

    Continuando os ajustes no painel de intro

---
+++ MainWorld/Assets/Scripts/configVideo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class configVideo : MonoBehaviour
+{
+
+    public VideoPlayer videoPlay;
+    public string videoClipName;
+    private string url;
+
+    private void Awake()
+    {
+        url = System.IO.Path.Combine(Application.streamingAssetsPath, videoClipName);
+        videoPlay.url = url;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!videoPlay.isPlaying)
+        {
+            videoPlay.Play();
+        }
+        else if (videoPlay.isPlaying)
+        {
+            return;
+        }
+    }
+}
