5036bf9c91664cdfdd33cee3eabf6917bfbe1cc9
Hitalo Alves <hitalo.ralves@hotmail.com>
2020-12-01 15:34:15 -0300

    Adicionando scripts para salvar perguntas e respostas no banco

---
+++ MainWorld/Assets/Scripts/BANCO/Model/Pergunta.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pergunta
+{
+    private int id;
+    private string descricao_pergunta1;
+    private string descricao_pergunta2;
+    private string descricao_pergunta3;
+    private int id_fase;
+
+    public Pergunta() { }
+
+    public Pergunta(int p_id, string p_pergunta1, string p_pergunta2, string p_pergunta3, int p_id_fase)
+    {
+        this.id = p_id;
+        this.descricao_pergunta1 = p_pergunta1;
+        this.descricao_pergunta2 = p_pergunta2;
+        this.descricao_pergunta3 = p_pergunta3;
+        this.id_fase = p_id_fase;
+    }
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Descricao_pergunta1
+    {
+        get { return descricao_pergunta1; }
+        set { descricao_pergunta1 = value; }
+    }
+
+    public string Descricao_pergunta2
+    {
+        get { return descricao_pergunta2; }
+        set { descricao_pergunta2 = value; }
+    }
+
+    public string Descricao_pergunta3
+    {
+        get { return descricao_pergunta3; }
+        set { descricao_pergunta3 = value; }
+    }
+
+    public int Id_fase
+    {
+        get { return id_fase; }
+        set { id_fase = value; }
+    }
+}
---
+++ MainWorld/Assets/Scripts/BANCO/Model/Resposta.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Resposta
+{
+    private int id;
+    private string resposta_pergunta1;
+    private string resposta_pergunta2;
+    private string resposta_pergunta3;
+    private int pergunta_id;
+
+    public Resposta() { }
+
+    public Resposta(int p_id, string p_resposta_pergunta1, string p_resposta_pergunta2, string p_resposta_pergunta3, int p_pergunta_id)
+    {
+        this.id = p_id;
+        this.resposta_pergunta1 = p_resposta_pergunta1;
+        this.resposta_pergunta2 = p_resposta_pergunta2;
+        this.resposta_pergunta3 = p_resposta_pergunta3;
+        this.pergunta_id = p_pergunta_id;
+    }
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Resposta_pergunta1
+    {
+        get { return resposta_pergunta1; }
+        set { resposta_pergunta1 = value; }
+    }
+
+    public string Resposta_pergunta2
+    {
+        get { return resposta_pergunta2; }
+        set { resposta_pergunta2 = value; }
+    }
+
+    public string Resposta_pergunta3
+    {
+        get { return resposta_pergunta3; }
+        set { resposta_pergunta3 = value; }
+    }
+
+    public int Pergunta_id
+    {
+        get { return pergunta_id; }
+        set { pergunta_id = value; }
+    }
+}
